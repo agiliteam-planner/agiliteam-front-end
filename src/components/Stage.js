b49d3c8ba8bed8d9dd4c5c68c18e7860789e081f
osadezu <osadezu@gmail.com>
2022-01-25 10:52:33 -0500

    Create React App with BrowserRouter and empty components

---
+++ src/components/Stage.js
@@ -0,0 +1,13 @@
+import { Route, Link } from 'react-router-dom';
+
+import TaskCard from './TaskCard';
+
+function Stage(props) {
+  return (
+    <div>
+      <TaskCard />
+    </div>
+  );
+}
+
+export default Stage;
---
+++ src/components/TaskCard.js
@@ -0,0 +1,7 @@
+import { Route, Link } from 'react-router-dom';
+
+function Stage(props) {
+  return <div></div>;
+}
+
+export default Stage;
