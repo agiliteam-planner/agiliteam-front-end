1eeef937c7b11e59ae9430e5bd7f8972b0968e76
osadezu <osadezu@gmail.com>
2022-01-25 11:35:41 -0500

    Rename CardDetails component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import { Routes, Route, NavLink } from 'react-router-dom';
 
 import Stages from './components/Stages';
-import CardDetails from './components/CardDetails';
+import TaskDetails from './components/TaskDetails';
 import About from './components/About';
 import Settings from './components/Settings';
+import Navigation from './components/Navigation';
 
 import './App.css';
 
 function App() {
   return (
     <div className='app'>
-      <header className='header'>
-        <h1>AgiliTeam</h1>
-        <ul>
-          <li>
-            <NavLink
-              to='settings'
-              // Apply following linesto all NavLinks when defining link styling
-              // className={({ isActive }) =>
-              //   isActive ? activeClassName : undefined
-              // }
-            >
-              Settings
-            </NavLink>
-            <NavLink to='about'>About</NavLink>
-          </li>
-        </ul>
-      </header>
       <Routes>
         <Route path='/' element={<Stages />} />
-        <Route path='/details' element={<CardDetails />} />
-        <Route path='/new' element={<CardDetails createNew={true} />} />
+        <Route path='/task/:id' element={<TaskDetails />} />
+        <Route path='/task/new' element={<TaskDetails createNew={true} />} />
         <Route path='/about' element={<About />} />
         <Route path='/settings' element={<Settings />} />
       </Routes>
