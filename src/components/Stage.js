dbf0292eab74b2a05ab818938ed27231f6ddb570
osadezu <osadezu@gmail.com>
2022-01-25 12:57:14 -0500

    Render basic Stages, Stage and TaskCard

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -2,10 +2,13 @@ import { Route, Link } from 'react-router-dom';
 
 import TaskCard from './TaskCard';
 
-function Stage(props) {
+function Stage({ stage }) {
   return (
-    <div>
-      <TaskCard />
+    <div className='stage'>
+      <div>{stage.name}</div>
+      {stage.tasks.map((task, i) => (
+        <TaskCard key={task._id} task={task} />
+      ))}
     </div>
   );
 }
--- src/components/Stages.js
+++ src/components/Stages.js
@@ -1,15 +1,84 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import Stage from './Stage';
 
 function Stages(props) {
-  const [stages, setStages] = useState([]);
+  // TEMP: Hard coded stages and tasks while building UI
+  const tempTasks = [
+    {
+      _id: 'abc',
+      _idtitle: 'Make wireframes',
+      description: 'Prepare wireframe for each view',
+      stage: 'Todo',
+      dueDate: 'Tue Jan 25 2022 10:12:52 GMT-0500',
+      priority: 2,
+      owner: 'eladsadeh',
+      checklist: [{ title: 'Main View', checked: false }],
+      comments: [
+        {
+          user: 'kurt',
+          content: 'lets do it',
+          time: 'Mon Jan 24 2022 9:12:52 GMT-0500',
+        },
+      ],
+      files: ['planning.md'],
+    },
+    {
+      _id: 'def',
+      title: 'Create Task Schema',
+      description: 'Prepare Task Schema based on project proposal definition',
+      stage: 'In Progress',
+      dueDate: 'Tue Jan 25 2022 10:12:52 GMT-0500',
+      priority: 2,
+      owner: 'kurt',
+      checklist: [null],
+      comments: [
+        {
+          user: 'kurt',
+          content: 'Almost done',
+          time: 'Tue Jan 25 2022 9:12:52 GMT-0500',
+        },
+      ],
+      files: ['Task.js'],
+    },
+    {
+      _id: 'ghi',
+      title: 'Main view React component',
+      description: 'Build the stages view in React',
+      stage: 'In Progress',
+      dueDate: 'Wed Jan 26 2022 18:00:00 GMT-0500',
+      priority: 2,
+      owner: 'oscar',
+      checklist: [{ title: 'Render stages', checked: true }],
+      comments: [
+        {
+          user: 'Oscar',
+          content: 'On it',
+          time: 'Tue Jan 25 2022 9:12:52 GMT-0500',
+        },
+      ],
+      files: ['Stages.js'],
+    },
+  ];
+
+  const tempInit = [
+    { name: 'To Do', tasks: tempTasks },
+    { name: 'In Progress', tasks: tempTasks },
+    { name: 'In Review', tasks: tempTasks },
+    { name: 'Done', tasks: tempTasks },
+  ];
+
+  // State for stages and tasks
+  const [stages, setStages] = useState(tempInit);
+
+  // On initial mount
+  useEffect(() => {}, []);
 
   return (
     <main>
       <div>
-        <Link to='new'>New Task</Link>
+        <Link to='task/new'>New Task</Link>
         <ul>
           {/* Each filter adds search params? to be bookmarkable */}
           <li>
@@ -21,9 +90,20 @@ function Stages(props) {
           <li>
             <button>Filter</button>
           </li>
+          {/* <li>
+            <label htmlFor="sort">Sort: </label>
+            <select name="sort" id="sort">
+              <option value="priority">Priority</option>
+              <option value="due-date">Due Date</option>
+            </select>
+          </li> */}
         </ul>
       </div>
-      <div>{stages.forEach}</div>
+      <div>
+        {stages.map((stage, i) => (
+          <Stage key={i} stage={stage} />
+        ))}
+      </div>
     </main>
   );
 }
