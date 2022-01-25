6d5e1786281a03c4f2ab234b3d01363bd7d1af26
osadezu <osadezu@gmail.com>
2022-01-25 12:57:14 -0500

    Render basic Stages, Stage and TaskCard

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,7 +1,31 @@
 import { Route, Link } from 'react-router-dom';
 
-function Stage(props) {
-  return <div></div>;
+const tempTask = {
+  _id: 'abc',
+  title: 'Make wireframes',
+  description: 'Prepare wireframe for each view',
+  stage: 'Todo',
+  dueDate: 'Tue Jan 25 2022 10:12:52 GMT-0500',
+  priority: 2,
+  owner: 'eladsadeh',
+  checklist: [{ title: 'Main View', checked: false }],
+  comments: [
+    {
+      user: 'kurt',
+      content: 'lets do it',
+      time: 'Mon Jan 24 2022 9:12:52 GMT-0500',
+    },
+  ],
+  files: ['planning.md'],
+};
+
+function TaskCard({ task }) {
+  return (
+    <div>
+      <p>{task.title}</p>
+      <Link to={`/task/${task._id}`} />
+    </div>
+  );
 }
 
-export default Stage;
+export default TaskCard;
