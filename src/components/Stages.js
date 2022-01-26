7e29f62fb212d21bad828626fcc6421cbc51e73e
osadezu <osadezu@gmail.com>
2022-01-25 19:13:15 -0500

    Load tasks from API and sort into stages

--- src/components/Stages.js
+++ src/components/Stages.js
@@ -6,11 +6,11 @@ import Stage from './Stage';
 import '../styles/Stages.css';
 
 function Stages(props) {
-  // TEMP: Hard coded stages and tasks while building UI
+  // TEMP: Hard coded tasks while building UI
   const tempTasks = [
     {
       _id: 'abc',
-      _idtitle: 'Make wireframes',
+      title: 'Dummy task 1',
       description: 'Prepare wireframe for each view',
       stage: 'Todo',
       dueDate: 'Tue Jan 25 2022 10:12:52 GMT-0500',
@@ -28,7 +28,7 @@ function Stages(props) {
     },
     {
       _id: 'def',
-      title: 'Create Task Schema',
+      title: 'Dummy task 2',
       description: 'Prepare Task Schema based on project proposal definition',
       stage: 'In Progress',
       dueDate: 'Tue Jan 25 2022 10:12:52 GMT-0500',
@@ -46,7 +46,7 @@ function Stages(props) {
     },
     {
       _id: 'ghi',
-      title: 'Main view React component',
+      title: 'Dummy task 3',
       description: 'Build the stages view in React',
       stage: 'In Progress',
       dueDate: 'Wed Jan 26 2022 18:00:00 GMT-0500',
@@ -63,23 +63,47 @@ function Stages(props) {
       files: ['Stages.js'],
     },
   ];
-  const tempInit = [
-    { name: 'To Do', tasks: tempTasks },
+
+  // Hardcoded stages to show all stages even if empty
+  const stages = [
+    { name: 'Todo', tasks: tempTasks },
     { name: 'In Progress', tasks: tempTasks },
     { name: 'In Review', tasks: tempTasks },
     { name: 'Done', tasks: tempTasks },
   ];
 
-  // State for stages and tasks
-  const [stages, setStages] = useState(tempInit);
+  // State for stages and their tasks
+  const [stagedTasks, setStagedTasks] = useState(stages);
+
+  // Sorts all tasks from API request into each stage
+  function updateTasks(tasks) {
+    const newStagedTasks = stages.map((stage) => {
+      const stageTasks = tasks.filter((task) => task.stage === stage.name);
+      return { name: stage.name, tasks: stageTasks };
+    });
+    setStagedTasks(newStagedTasks);
+  }
 
   // On initial mount
-  useEffect(() => {}, []);
+  useEffect(() => {
+    // TODO: Move url to a settings location or env variable?
+    const backendUrl = 'https://arcane-plateau-58687.herokuapp.com';
+
+    // IIFE to make it async
+    (async () => {
+      try {
+        const response = await fetch(backendUrl + '/tasks');
+        const tasks = await response.json();
+        updateTasks(tasks);
+      } catch (error) {}
+    })();
+  }, []);
 
   return (
     <main>
       <div>
         <Link to='/task/new'>New Task</Link>
+
         <ul>
           {/* Each filter adds search params? to be bookmarkable */}
           <li>
@@ -101,7 +125,7 @@ function Stages(props) {
         </ul>
       </div>
       <div className='stages-container'>
-        {stages.map((stage, i) => (
+        {stagedTasks.map((stage, i) => (
           <Stage key={i} stage={stage} />
         ))}
       </div>
