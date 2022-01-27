7086c220b57e2601efe8797a6998c3e5332d02c3
osadezu <osadezu@gmail.com>
2022-01-27 08:27:55 -0500

    Implement ssettings user editing

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,75 +3,81 @@ import axios from 'axios';
 
 import User from './User';
 
+import '../styles/Users.css';
+
 function Users(props) {
-  const [users, setUsers] = useState([]);
-  const [edits, setEdits] = useState({});
-  const [editingUser, setEditingUser] = useState(null);
+	const [users, setUsers] = useState([]);
+	const [edits, setEdits] = useState({});
+	const [editingUser, setEditingUser] = useState(null);
 
-  // TODO: Move url to a settings location or env variable?
-  const backendUrl = 'https://arcane-plateau-58687.herokuapp.com';
+	// TODO: Move url to a settings location or env variable?
+	const backendUrl = 'https://arcane-plateau-58687.herokuapp.com';
 
-  // On initial mount
-  useEffect(() => {
-    // IIFE to make it async
-    (async () => {
-      try {
-        const response = await axios.get(`${backendUrl}/users`);
-        setUsers(response.data);
-      } catch (error) {
-        // TODO: Handle errors for user
-        console.error(error);
-      }
-    })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+	// On initial mount
+	useEffect(() => {
+		// IIFE to make it async
+		(async () => {
+			try {
+				const response = await axios.get(`${backendUrl}/users`);
+				setUsers(response.data);
+			} catch (error) {
+				// TODO: Handle errors for user
+				console.error(error);
+			}
+		})();
+	}, [editingUser]);
 
-  // Change editing user state if no other editing is happening
-  function handleRowClick(userID) {
-    if (!editingUser) {
-      setEditingUser(userID);
-      const userToEdit = users.find((user) => user._id === userID);
-      setEdits({ ...userToEdit });
-    } // else do nothing
-  }
+	// Change editing user state if no other editing is happening
+	function handleRowClick(userID) {
+		if (!editingUser) {
+			setEditingUser(userID);
+			const userToEdit = users.find((user) => user._id === userID);
+			setEdits({ ...userToEdit });
+		} // else do nothing
+	}
 
-  function handleChange(e) {
-    setEdits({ ...edits, [e.target.id]: e.target.value });
-  }
+	function handleChange(e) {
+		setEdits({ ...edits, [e.target.id]: e.target.value });
+	}
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(edits);
-    axios.put(`${backendUrl}/users/${edits._id}`);
-  }
+	function handleSubmit(e) {
+		e.preventDefault();
+		console.log(edits);
+		axios
+			.put(`${backendUrl}/users/${edits._id}`, edits)
+			.then((res) => console.log(res));
 
-  return (
-    <div>
-      <div className='settings-users'>
-        <h3>Manage Users</h3>
-        <form id='edit-users' action='' onSubmit={handleSubmit}>
-          <table className='users-table'>
-            <thead>
-              {/* Render the <th></th> from User component so that all logic for table columns lives in User.js */}
-              <User user='header' />
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <User
-                  key={user._id}
-                  user={user}
-                  editingUser={editingUser}
-                  edits={edits}
-                  handleRowClick={handleRowClick}
-                  handleChange={handleChange}
-                />
-              ))}
-            </tbody>
-          </table>
-        </form>
-      </div>
-    </div>
-  );
+		setEditingUser(null);
+		setEdits({});
+	}
+
+	return (
+		<div>
+			<div className='settings-users'>
+				<h3>Manage Users</h3>
+				<form id='edit-users' action='' onSubmit={handleSubmit}>
+					<table className='users-table'>
+						<thead>
+							{/* Render the <th></th> from User component so that all logic for table columns lives in User.js */}
+							<User user='header' />
+						</thead>
+						<tbody>
+							{users.map((user) => (
+								<User
+									key={user._id}
+									user={user}
+									editingUser={editingUser}
+									edits={edits}
+									handleRowClick={handleRowClick}
+									handleChange={handleChange}
+								/>
+							))}
+						</tbody>
+					</table>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Users;
