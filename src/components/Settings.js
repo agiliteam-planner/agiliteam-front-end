86d7a7d1c3d9237634bd10315095d6170809ec6b
osadezu <osadezu@gmail.com>
2022-01-27 16:48:21 -0500

    Complete CRUD fuctionality for users and basic styling

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,13 +1,15 @@
 import Users from './Users';
 
+import '../styles/Settings.css';
+
 function Settings(props) {
-  return (
-    <>
-      <h2>Settings</h2>
-      <Users />
-      {/* look at <Users /> top div to match styling when adding other Settings panels */}
-    </>
-  );
+	return (
+		<>
+			<h2>Settings</h2>
+			<Users />
+			{/* look at <Users /> top div to match styling when adding other Settings panels */}
+		</>
+	);
 }
 
 export default Settings;
