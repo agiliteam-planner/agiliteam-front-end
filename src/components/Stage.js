6e843231b1367f623e2163189fca8c27c3fde49e
osadezu <osadezu@gmail.com>
2022-01-29 23:34:26 -0500

    Update styles progress

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -5,12 +5,11 @@ import '../styles/Stage.css';
 function Stage({ stage }) {
 	return (
 		<div className='stage'>
-			<h3 className='stage-title'>{stage.name}</h3>
-			{/* <div className='task-list'> */}
+			<h3 className='stage-name'>{stage.name}</h3>
 			{stage.tasks.map((task, i) =>
 				task._id ? <TaskCard key={task._id} task={task} /> : task.title
 			)}
-			{/* </div> */}
+			{/* <div className='stage-footer'></div> */}
 		</div>
 	);
 }
